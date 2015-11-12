9f73f3c2da81e91e29e583631ee3be26baa2a927
Henry Hazan <henry@teramine.io>
2015-11-12 12:23:23 +0200

    first commit

---
+++ intro/Pet.java
@@ -0,0 +1,20 @@
+/*
+ Author: Matthew Might
+ Site:   http://matt.might.net/
+         http://www.ucombinator.org/
+
+ This is a companion file for DemoScalaPet.scala
+ */
+
+
+public class Pet {
+    private String name ;
+    
+    public Pet(String name) {
+        this.name = name ;
+    }
+
+    public String getName() {
+        return name ;
+    }
+}
